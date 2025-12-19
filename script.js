e9b0d2e794e554cef6ae4e6c709c86cab6f90865
shrey767 <shreyasb915@gmail.com>
2025-12-19 16:57:03 +0530

    mostly working board

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
 // DEBUG: confirm script loads and element selection
 
-const button = document.getElementById("btn");
 
 function checkBoard(boardState,n){
-    
+    for(let i=0;i < n; i++){
+        for(let j=0; j < n; j++){
+            if(boardState[i][j]!=null){
+                if(boardState[i][j]==boardState[i][j+1] && boardState[i][j+1]==boardState[i][j+2]){
+                    const winner = boardState[i][j];
+                    winnerDeclare.textContent=`Player ${boardState[i][j]} wins!`;
+                    setTimeout(() => {resetButton.click();}, 1000);
+                    return;
+                }
+                else if(boardState[i][j]==boardState[i+1][j] && boardState[i+1][j]==boardState[i+2][j]){
+                    const winner = boardState[i][j];
+                    winnerDeclare.textContent=`Player ${boardState[i][j]} wins!`;
+                    setTimeout(() => {resetButton.click();}, 1000);
+                    return;
+                }
+                else if(boardState[i][j]==boardState[i+1][j+1] && boardState[i+1][j+1]==boardState[i+2][j+2]){
+                    const winner = boardState[i][j];
+                    winnerDeclare.textContent=`Player ${boardState[i][j]} wins!`;
+                    setTimeout(() => {resetButton.click();}, 1000);
+                    return;
+                }
+                else if(boardState[i][j]==boardState[i+1][j-1] && boardState[i+1][j-1]==boardState[i+2][j-2]){
+                    const winner = boardState[i][j];
+                    winnerDeclare.textContent=`Player ${boardState[i][j]} wins!`;
+                    setTimeout(() => {resetButton.click();}, 1000);
+                    return;
+                }
+            }
+        }
+
+    }
 }
 
 let player = 'X';
 
-const n = 4; // change this to any size (3, 4, 5, 10...)
+const n = 3; // change this to any size (3, 4, 5, 10...)
 const boardState = Array.from({ length: n }, () =>
   Array(n).fill(null)
 );
 
 const board = document.getElementById("board");
-
+const cells = Array.from({ length: n }, () =>
+  Array(n).fill(null)
+);
 // tell CSS grid how many rows & columns
 board.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
 board.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+let currentPlayer = document.getElementById("player");
 
-// create n^2 buttons
 for (let i = 0; i < n; i++) {
     for(let j=0; j < n;  j++){
         const cell = document.createElement("button");
@@ -27,7 +58,6 @@ for (let i = 0; i < n; i++) {
         cell.textContent = ""; // later: X or O
         cell.addEventListener("click", () => {
             cell.classList.add("clicked");
-            let currentPlayer = document.getElementById("player");
             if (cell.textContent===""){
                 cell.textContent=player;
                 boardState[i][j]=player;
@@ -39,7 +69,23 @@ for (let i = 0; i < n; i++) {
                 }
                 currentPlayer.textContent=`To play: ${player}`;
             }
+            checkBoard(boardState,n);
         });
         board.appendChild(cell);
+        cells[i][j]=cell;
 }}
+const winnerDeclare = document.getElementById("winner");
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", () => {
+    winnerDeclare.textContent=""
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n; j++){
+            cells[i][j].textContent="";
+            boardState[i][j]=null;
+            cells[i][j].classList.remove("clicked");
+            player="X";
+            currentPlayer.textContent=`To play: ${player}`;
+        }
+    }
+});
 
